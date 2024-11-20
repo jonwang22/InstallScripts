9d2cbc353bee63b81dc1fe0a468975d5f425bd6c
Jon Wang <155007142+jonwang22@users.noreply.github.com>
2024-11-20 12:21:54 -0500

    Update install_prometheus.sh

--- install_prometheus.sh
+++ install_prometheus.sh
@@ -41,24 +41,6 @@ sudo systemctl daemon-reload
 sudo systemctl start prometheus
 sudo systemctl enable prometheus
 
-# Install Grafana
-# Add Grafana GPG key and repository
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
-
-# Update package list and install Grafana
-sudo apt update -y
-sudo apt install grafana -y
-
-# Start and enable Grafana service
-sudo systemctl start grafana-server
-sudo systemctl enable grafana-server
-
-# Print the public IP address
-echo "Installation complete. Access Prometheus at http://$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4):9090"
-echo "Access Grafana at http://$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4):3000"
-echo "Default Grafana login is admin/admin"
-
 # Add Node Exporter job to Prometheus config
 cat << EOF | sudo tee -a /opt/prometheus/prometheus.yml
 
